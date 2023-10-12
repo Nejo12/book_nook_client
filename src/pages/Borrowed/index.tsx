52a956caf3ea6d3bef465c78733b812eecdf16d7
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-10-12 14:55:41 +0200

    refactor(style): improve ui display on longer screen
    - page-titles display has poor display on rotated mobile screens, thereby remove page title on all pages.
    
    - remove forgotten comments.
    
    - remove hover effect on card-container

--- src/pages/Borrowed/index.tsx
+++ src/pages/Borrowed/index.tsx
@@ -23,7 +23,7 @@ const Borrowed = () => {
 
   return (
     <div className='page-container p2'>
-      <p className='page-title lead emboss hide-md'>Borrowed List</p>
+      {/* <p className='page-title lead emboss hide-md'>Borrowed List</p> */}
       {loading ? (
         <Spinner />
       ) : (
